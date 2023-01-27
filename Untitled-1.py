c453b86ecfd9fe560714fa2ff0c778085fd62a34
marciobarrosbr <mf.barros84@gmail.com>
2023-01-27 09:32:32 -0300

    01 exercicio em py
    exercercio que deu certo

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,31 @@
-000
+quiz = {
+    "question1": {
+        "question": "what is the capital of France?",
+        "answer": "Paris"
+    },
+    "question2": {
+    "question": "What is the capital of Germany",
+    "answer": "Berlin"
+    },
+}
+score = 0
+
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input("Answer?")
+    
+    if answer.lower() == value[ 'answer'] .lower():
+        print('Correct')
+        score = score + 1
+        print("Your score is: " + str(score))
+        print("")
+        print("")
+    else:
+        print("Wrong!")
+        print('The answer is :' + value[ 'answer'])
+        print("Your score is: "+ str(score))
+        print("")
+
+print("You got " + str(score) + "out of 2 question correctly")
+
+print("Your percentage is " + str(int(score/7 * 100)) +"%")
