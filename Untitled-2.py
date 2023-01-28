62ac6a80a2dc9013cb0c4704825478351d88cfea
marciobarrosbr <mf.barros84@gmail.com>
2023-01-28 12:27:11 -0300

    nova atualização

--- Untitled-2.py
+++ Untitled-2.py
@@ -20,7 +20,7 @@
 #name = "Bro"
 #age = 21
 #attractive = True
-name, age, attractive = "bro", 21, True
+#name, age, attractive = "bro", 21, True
 
 #print(name)
 #rint(age)
@@ -30,6 +30,16 @@ name, age, attractive = "bro", 21, True
 #Patrick = 30
 #Sandy = 30
 #Squidward = 30
-Spongebob = patrick = Sandy = Squidward = 30
+#Spongebob = patrick = Sandy = Squidward = 30
 
-print(Spongebob)
+#print(Spongebob)
+
+#STRING METHODS
+
+name = "Bro Code"
+#print(name.find(name)) #find() indica o posicionamento do caractere dentro da cadeia, no caso “Bro Code” ‘B’ está na primeira posição que é zero#
+#print(name.capitalize()) #capitalize() poe o primeirop caracter em caixa alta#
+#print(name.upper()) #upper() apresenta toda a cadeia de caracter em caixa alta#
+#print(name.lower()) #lower() apresenta toda a cadeia de carater em caixa baixa"
+#print(name.isdigit()) #isdigit() retorna o resultado logico 'booleano' que so é verdadeiro quando a cadeia é foramada somente por numeros, sem espaços letras ou qualquer tipo de caracter
+#print(name.isalpha()) #isalpla() retorna o resultado logico 'booleano' que so é verdadeiro quando a cadeia é foramada somente por lestras do alfaberto, sem espaços numeros ou qualquer tipo de caracter
