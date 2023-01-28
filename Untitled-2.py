a3ad89926475b06ab0c4eccac12c8dab7174a1f5
marciobarrosbr <mf.barros84@gmail.com>
2023-01-28 10:47:20 -0300

    novo Untitled
    conten alguns exercicios

---
+++ Untitled-2.py
@@ -0,0 +1,35 @@
+#first_name = "bro"
+#print(type(first_name))
+#last_name = "code"
+#full_name = first_name+" " +last_name
+#print("hello "+full_name)
+
+#age = 21
+#age +=1
+#print(age)
+#print("yor age is: "+str(age))
+
+#height = 250.5
+#print("Your height is: "+str(height)+"cm")
+#print(type(height))
+
+#human = True
+#print(human)
+#print("are you a huma:"+str(human))
+
+#name = "Bro"
+#age = 21
+#attractive = True
+name, age, attractive = "bro", 21, True
+
+#print(name)
+#rint(age)
+#print(attractive)
+
+#spongebob = 30
+#Patrick = 30
+#Sandy = 30
+#Squidward = 30
+Spongebob = patrick = Sandy = Squidward = 30
+
+print(Spongebob)
