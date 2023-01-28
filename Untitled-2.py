f6950e007cbbc3084c32cb12a20c463a7d759446
marciobarrosbr <mf.barros84@gmail.com>
2023-01-28 20:10:08 -0300

    corrigido confito

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,3 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def binary_search(list, element):
     middle = 0
     start = 0
@@ -7,7 +51,7 @@ def binary_search(list, element):
     while(start<=end):
         print("Step", steps, ":" ,str(list[start:end+1]))
         steps = steps+1
-        middle = (star + end // 2)
+        middle = (start + end // 2)
 
         if element == list[middle]  :
             return middle
