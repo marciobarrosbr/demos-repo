03c639d4911bc2088f4e7a0a1dbbbf9b23632479
marciobarrosbr <mf.barros84@gmail.com>
2023-01-28 20:21:12 -0300

    unindo 02 codigos

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,46 +1,46 @@
+def add(a, b):
+    answer = a + b
+    print(str(a) + " + "  + str(b) + " = " + str(answer) + "\n")
+def sub(a, b):
+    answer = a - b
+    print(str(a) + " - " + str(b) + " = " + str(answer) + "\n")
+def mul(a, b):
+    answer = a * b
+    print(str(a) + " * " + str(b)  + " = " + str(answer) + "\n")
+def div(a, b):
+    answer = a / b
+    print(str(a) + " / " + str(b)  + " = " + str(answer) + "\n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while True:
+    print("A. Addition")
+    print("B. Subtration")
+    print("C. Multiplication")
+    print("D. Division")
+    print("E. Exit")
+    choice = input("input ypur choice: ")
+    if choice == "a" or choice == "A":
+        print("Addition")
+        a = int(input("input first number: "))
+        b = int(input("input second number: "))
+        add(a, b)
+    elif choice == "b" or choice == "B":
+        print("Subtraction")
+        a = int(input("input fist number: "))
+        b = int(input("input second number: "))
+        sub(a, b)
+    elif choice == "c" or choice == "C":
+        print("Multiplication")
+        a = int(input("input first number: "))
+        b = int(input("input second number: "))
+        mul(a, b)
+    elif choice == "d" or choice == "D":
+        print("Division")
+        a = int(input("input firs number: "))
+        b = int(input("Input second number: "))
+        div(a, b)
+    elif choice == "e" or choice == "E":
+        print("progran ended")
+        quit()
 
 def binary_search(list, element):
     middle = 0
